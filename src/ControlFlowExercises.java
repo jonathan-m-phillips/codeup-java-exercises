653c7c9d3a6ff7607a73267d4c49e2831a40494e
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-10 16:29:28 -0500

    ControlFlowExercises problem 2, 8/10

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,51 +5,65 @@ public class ControlFlowExercises {
 
 
         // a: While
-        int i = 5;
+//        int i = 5;
+//
+//        while(i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//
+//        // b: Do While
+//        int j = 0;
+//        do {
+//            System.out.println(j);
+//            j += 2;
+//        } while (j <= 100);
+//
+//        int k = 100;
+//        do {
+//            System.out.println(k);
+//            k -= 5;
+//        } while (k >=-10);
+//
+//        long m = 2;
+//        do {
+//            System.out.println(m);
+//            m = m * m;
+//        } while(m <= 1000000);
+//
+//
+//        // c: For
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.println(i);
+//        }
+//
+//        for(int i = 0; i <= 100; i += 2){
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+//
+//        for (long i = 2; i <= 1000000; i *= i) {
+//            System.out.println(i);
+//        }
 
-        while(i <= 15) {
-            System.out.println(i);
-            i++;
+        // 2: FizzBuzz
+        for (int i = 1; i <= 100; i++) {
+            if(i % 3 == 0) {
+                if (i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Fizz");
+                }
+            } else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
-
-
-        // b: Do While
-        int j = 0;
-        do {
-            System.out.println(j);
-            j += 2;
-        } while (j <= 100);
-
-        int k = 100;
-        do {
-            System.out.println(k);
-            k -= 5;
-        } while (k >=-10);
-
-        long m = 2;
-        do {
-            System.out.println(m);
-            m = m * m;
-        } while(m <= 1000000);
-
-
-        // c: For
-        for (int i = 5; i <= 15; i++) {
-            System.out.println(i);
-        }
-
-        for(int i = 0; i <= 100; i += 2){
-            System.out.println(i);
-        }
-
-        for (int i = 100; i >= -10; i -= 5) {
-            System.out.println(i);
-        }
-
-        for (long i = 2; i <= 1000000; i *= i) {
-            System.out.println(i);
-        }
-
 
 
     }
