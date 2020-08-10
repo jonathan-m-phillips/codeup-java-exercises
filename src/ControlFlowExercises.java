ef40bdf769c7d9652619d44f2a3839159a33fbfb
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-10 16:11:49 -0500

    ControlFlowExercises problem 1, 8/10

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,56 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        // 1: Loop Basics
+
+
+        // a: While
+        int i = 5;
+
+        while(i <= 15) {
+            System.out.println(i);
+            i++;
+        }
+
+
+        // b: Do While
+        int j = 0;
+        do {
+            System.out.println(j);
+            j += 2;
+        } while (j <= 100);
+
+        int k = 100;
+        do {
+            System.out.println(k);
+            k -= 5;
+        } while (k >=-10);
+
+        long m = 2;
+        do {
+            System.out.println(m);
+            m = m * m;
+        } while(m <= 1000000);
+
+
+        // c: For
+        for (int i = 5; i <= 15; i++) {
+            System.out.println(i);
+        }
+
+        for(int i = 0; i <= 100; i += 2){
+            System.out.println(i);
+        }
+
+        for (int i = 100; i >= -10; i -= 5) {
+            System.out.println(i);
+        }
+
+        for (long i = 2; i <= 1000000; i *= i) {
+            System.out.println(i);
+        }
+
+
+
+    }
+}
