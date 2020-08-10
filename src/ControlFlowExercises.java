e7c3707480b9e4222573c119c663136a6bf4d14c
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-10 18:01:55 -0500

    finished exercise, ControlFlowExercises, 8/10

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -69,31 +69,66 @@ public class ControlFlowExercises {
 //        }
 
         // 3: Display a table of powers
-        boolean userInput = true;
+//        boolean userInput = true;
+//
+//        do {
+//
+//            System.out.println("What number would you like to go up to? ");
+//            String num1 = scanner.next();
+//            int userNum = Integer.parseInt(num1);
+//            int number = 1;
+//
+//            System.out.println("\nnumber | squared | cubed\n------ | ------- | -----");
+//
+//            do {
+//                int squared = number * number;
+//                int cubed = number * number * number;
+//                System.out.printf("%d\t   | %d\t     | %d\n", number, squared, cubed);
+//                number++;
+//            } while (number <= userNum);
+//
+//            System.out.println("\nDo you want to give another number? y/n");
+//            String playAgain = scanner.next();
+//            if (playAgain.equals("n")) {
+//                userInput = false;
+//            }
+//
+//        } while (userInput);
+
+
+        // 4: Convert given number grades into letter grades
+        boolean userAgrees = true;
+        String letterA = "A";
+        String letterB = "B";
+        String letterC = "C";
+        String letterD = "D";
+        String letterF = "F";
 
         do {
 
-            System.out.println("What number would you like to go up to? ");
-            String num1 = scanner.next();
-            int userNum = Integer.parseInt(num1);
-            int number = 1;
-
-            System.out.println("\nnumber | squared | cubed\n------ | ------- | -----");
-
-            do {
-                int squared = number * number;
-                int cubed = number * number * number;
-                System.out.printf("%d\t   | %d\t     | %d\n", number, squared, cubed);
-                number++;
-            } while (number <= userNum);
-
-            System.out.println("\nDo you want to give another number? y/n");
-            String playAgain = scanner.next();
-            if (playAgain.equals("n")) {
-                userInput = false;
+            System.out.println("Enter your numerical grade: ");
+            String userGrade = scanner.next();
+            int grade = Integer.parseInt(userGrade);
+            if (grade <= 100 && grade >= 88) {
+                System.out.printf("%d = %s", grade, letterA);
+            } else if (grade <= 87 && grade >= 80) {
+                System.out.printf("%d = %s", grade, letterB);
+            } else if (grade <= 79 && grade >= 67) {
+                System.out.printf("%d = %s", grade, letterC);
+            } else if (grade <= 66 && grade >= 60) {
+                System.out.printf("%d = %s", grade, letterD);
+            } else if (grade >= 0) {
+                System.out.printf("%d = %s", grade, letterF);
             }
 
-        } while (userInput);
+            System.out.println("\nDo you want to enter another grade? y/n ");
+            String userContinue = scanner.next();
+
+            if (userContinue.equals("n")) {
+                userAgrees = false;
+            }
+
+        } while (userAgrees);
 
     }
 }
