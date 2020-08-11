652a8d0dccd7497090b9119106425a4bfab96e7f
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-11 14:06:56 -0500

    Update to StringExercise, 8/11

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,14 +2,14 @@ public class StringExercise {
     public static void main(String[] args) {
 
         // 1: String Basics
-        String stringBasics1 = "We don't need no education\nWe don't need no thought control";
-        System.out.println(stringBasics1);
+        String stringBasics = "We don't need no education\nWe don't need no thought control";
+        System.out.println(stringBasics);
 
-        String stringBasics2 = "\nCheck \"this\" out!, \"s inside of \"s!";
-        System.out.println(stringBasics2);
+        stringBasics = "\nCheck \"this\" out!, \"s inside of \"s!";
+        System.out.println(stringBasics);
 
-        String stringBasics3 = "\nIn windows, the main drive is usually C:\\";
-        System.out.println(stringBasics3);
+        stringBasics = "\nIn windows, the main drive is usually C:\\";
+        System.out.println(stringBasics);
 
     }
 }
