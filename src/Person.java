6d761bcc2f60bb4fb83026698a5aab2f58f1a67f
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-13 17:36:43 -0500

    Person class updated, 8/13

--- src/Person.java
+++ src/Person.java
@@ -9,14 +9,14 @@ public class Person {
     public void setName(String name){
         this.name = name;
     }
-    public String sayHello(){
-        return name;
+
+    public void sayHello(){
+        System.out.println(name);
     }
 
     public static void main(String[] args) {
         Person jon = new Person();
         jon.name = "Jon";
-        System.out.println(jon.sayHello());
+        jon.sayHello();
     }
-
 }
