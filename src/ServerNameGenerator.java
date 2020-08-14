6b22f17f45ce30c3db9b4ecb6f86bdbc143bdb72
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-14 17:02:56 -0500

    started ServerNameGenerator, 8/14

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class ServerNameGenerator {
+    Random rand = new Random();
+
+    public static void main(String[] args) {
+        String[] adj = {"big", "bald", "chubby", "fit", "plain", "witty", "fierce", "fat", "great", "itchy"};
+        String[] noun = {"designer", "doctor", "fox", "house", "fan", "laptop", "lung", "gift", "medicine", "gloves"};
+
+
+    }
+}
