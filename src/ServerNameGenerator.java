e6cbe4927acb8e834358e46be28418c2b4ce533c
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-17 09:29:22 -0500

    ServerNameGenerator, completed 8/17

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -2,12 +2,23 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class ServerNameGenerator {
-    Random rand = new Random();
 
-    public static void main(String[] args) {
+
+    public static void randomElement () {
         String[] adj = {"big", "bald", "chubby", "fit", "plain", "witty", "fierce", "fat", "great", "itchy"};
         String[] noun = {"designer", "doctor", "fox", "house", "fan", "laptop", "lung", "gift", "medicine", "gloves"};
+        Random rand = new Random();
+        int randAdjInt = rand.nextInt(adj.length);
+        String randAdj = adj[randAdjInt];
+        int randNounInt = rand.nextInt(noun.length);
+        String randNoun = noun[randNounInt];
 
+        System.out.printf("%s-%s", randAdj, randNoun);
+
+    }
+
+    public static void main(String[] args) {
+        randomElement();
 
     }
 }
