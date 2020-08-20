39388610312bb1ad034b40bc249985094bd3a1fb
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-20 16:44:20 -0500

    GroceryList minor changes needed for completion, 8/20

--- src/GroceryList.java
+++ src/GroceryList.java
@@ -15,8 +15,9 @@ public class GroceryList {
 
         boolean userAgrees = true;
 
-        do {
-            if (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")) {
+
+        if (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")) {
+            do {
                 System.out.println("Given an ordered list of grocery categories to choose from, select the category using numbers: ");
                 System.out.println("1) Meat\n2) Bread\n3) Drinks\n4) Vegetables");
                 userInput = scanner.nextLine();
@@ -27,7 +28,8 @@ public class GroceryList {
                     userInput = scanner.nextLine();
                     String userItem = userInput;
                     System.out.println("Quantity of this item?");
-                    int userItems = scanner.nextInt();
+                    userInput = scanner.nextLine();
+                    int userItems = Integer.parseInt(userInput);
                     items.put(userItem, userItems);
                     groceryList.put("meat", items);
                 } else if (userCategory == 2) {
@@ -35,7 +37,8 @@ public class GroceryList {
                     userInput = scanner.nextLine();
                     String userItem = userInput;
                     System.out.println("Quantity of this item?");
-                    int userItems = scanner.nextInt();
+                    userInput = scanner.nextLine();
+                    int userItems = Integer.parseInt(userInput);
                     items.put(userItem, userItems);
                     groceryList.put("bread", items);
                 } else if (userCategory == 3) {
@@ -43,7 +46,8 @@ public class GroceryList {
                     userInput = scanner.nextLine();
                     String userItem = userInput;
                     System.out.println("Quantity of this item?");
-                    int userItems = scanner.nextInt();
+                    userInput = scanner.nextLine();
+                    int userItems = Integer.parseInt(userInput);
                     items.put(userItem, userItems);
                     groceryList.put("drinks", items);
                 } else if (userCategory == 4) {
@@ -51,13 +55,16 @@ public class GroceryList {
                     userInput = scanner.nextLine();
                     String userItem = userInput;
                     System.out.println("Quantity of this item?");
-                    int userItems = scanner.nextInt();
+                    userInput = scanner.nextLine();
+                    int userItems = Integer.parseInt(userInput);
                     items.put(userItem, userItems);
                     groceryList.put("vegetables", items);
                 }
 
+
                 System.out.println("Would you like to finalize the list or add more items?\n1: finalize\n2: add more items");
-                int userInputs = scanner.nextInt();
+                String yesNo = scanner.nextLine();
+                int userInputs = Integer.parseInt(yesNo);
 
                 if (userInputs == 1) {
                     userAgrees = false;
@@ -65,18 +72,20 @@ public class GroceryList {
                     continue;
                 } else {
                     System.out.println("Would you like to finalize the list or add more items?\n1) finalize\n2: add more items");
-                    userInputs = scanner.nextInt();
+                    yesNo = scanner.nextLine();
+                    userInputs = Integer.parseInt(yesNo);
+                }
+
+
+            } while (userAgrees);
+        }
+
+            for (Map.Entry<String, HashMap<String, Integer>> entry : groceryList.entrySet()) {
+                for (Map.Entry<String, Integer> entry2 : items.entrySet()) {
+                    System.out.println(entry.getKey() + " : " + entry2.getKey() + ", " + entry2.getValue());
                 }
             }
 
-        } while (userAgrees);
-
-        for (Map.Entry<String, HashMap<String, Integer>> entry : groceryList.entrySet()) {
-            for (Map.Entry<String, Integer> entry2 : items.entrySet()) {
-                System.out.println(entry.getKey() + " : " + entry2.getKey() + ", " + entry2.getValue());
-            }
         }
 
     }
-
-}
