57185f9b53ecfbaf3351ec26a38ab24c43e41172
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-20 11:41:42 -0500

    Java Collections, grades exercise part 2 completed 8/20

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -2,6 +2,7 @@ package grades;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class GradesApplication {
 
@@ -12,7 +13,32 @@ public class GradesApplication {
         Student student4 = new Student("Elam Ferguson");
         HashMap<String, Student> students = new HashMap<>();
 
-        students.put("bob-dylan", student1);
+        students.put("og-cullen-bohannon", student1);
+        students.put("miss-lily-bell", student2);
+        students.put("doc-durant", student3);
+        students.put("mister-ferguson", student4);
+
+        student1.addGrade(99);
+        student1.addGrade(94);
+        student1.addGrade(97);
+
+        student2.addGrade(99);
+        student2.addGrade(100);
+        student2.addGrade(98);
+
+        student3.addGrade(71);
+        student3.addGrade(73);
+        student3.addGrade(80);
+
+        student4.addGrade(83);
+        student4.addGrade(88);
+        student4.addGrade(63);
+
+        for (Map.Entry<String, Student> entry: students.entrySet()) {
+            System.out.println("Github username: " + entry.getKey() + "\nName: " + entry.getValue().getName());
+        }
+
+
     }
 
 }
