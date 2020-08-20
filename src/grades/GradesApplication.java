4e5e83275fe23c4cd4fb9d48c0cefc550096ec06
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-20 15:26:15 -0500

    Java Collections, grades exercise completed 8/20

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -29,20 +29,70 @@ public class GradesApplication {
         student4.addGrade(88);
         student4.addGrade(63);
 
-
+        // HashMap for students    GitHub username, student name
         HashMap<String, Student> students = new HashMap<>();
         students.put("og-cullen-bohannon", student1);
         students.put("miss-lily-bell", student2);
         students.put("doc-durant", student3);
         students.put("mister-ferguson", student4);
 
+        //creating variables to store github usernames
         String cullen = "og-cullen-bohannon";
         String lily = "miss-lily-bell";
         String durant = "doc-durant";
         String ferguson = "mister-ferguson";
 
 
-        System.out.println(students.get("doc-durant").getName());
+        // student1 attendance record
+        student1.recordAttendance("2020-08-01", "p");
+        student1.recordAttendance("2020-08-02", "p");
+        student1.recordAttendance("2020-08-03", "p");
+        student1.recordAttendance("2020-08-04", "p");
+        student1.recordAttendance("2020-08-05", "p");
+        student1.recordAttendance("2020-08-08", "a");
+        student1.recordAttendance("2020-08-09", "p");
+        student1.recordAttendance("2020-08-10", "p");
+        student1.recordAttendance("2020-08-11", "p");
+
+        //student2 attendance record
+        student2.recordAttendance("2020-08-01", "p");
+        student2.recordAttendance("2020-08-02", "p");
+        student2.recordAttendance("2020-08-03", "p");
+        student2.recordAttendance("2020-08-04", "p");
+        student2.recordAttendance("2020-08-05", "p");
+        student2.recordAttendance("2020-08-06", "p");
+        student2.recordAttendance("2020-08-07", "p");
+        student2.recordAttendance("2020-08-08", "p");
+        student2.recordAttendance("2020-08-09", "p");
+        student2.recordAttendance("2020-08-10", "p");
+        student2.recordAttendance("2020-08-11", "p");
+
+        //student3 attendance record
+        student3.recordAttendance("2020-08-01", "p");
+        student3.recordAttendance("2020-08-02", "a");
+        student3.recordAttendance("2020-08-03", "p");
+        student3.recordAttendance("2020-08-04", "a");
+        student3.recordAttendance("2020-08-05", "p");
+        student3.recordAttendance("2020-08-06", "a");
+        student3.recordAttendance("2020-08-07", "p");
+        student3.recordAttendance("2020-08-08", "p");
+        student3.recordAttendance("2020-08-09", "a");
+        student3.recordAttendance("2020-08-10", "p");
+        student3.recordAttendance("2020-08-11", "p");
+
+        //student4 attendance record
+        student4.recordAttendance("2020-08-01", "a");
+        student4.recordAttendance("2020-08-02", "p");
+        student4.recordAttendance("2020-08-03", "p");
+        student4.recordAttendance("2020-08-04", "p");
+        student4.recordAttendance("2020-08-05", "p");
+        student4.recordAttendance("2020-08-06", "a");
+        student4.recordAttendance("2020-08-07", "p");
+        student4.recordAttendance("2020-08-08", "p");
+        student4.recordAttendance("2020-08-09", "a");
+        student4.recordAttendance("2020-08-10", "p");
+        student4.recordAttendance("2020-08-11", "p");
+
 
         // looping to add all the github usernames to a single variable for output
         String gitHubUsernames = "";
@@ -77,19 +127,27 @@ public class GradesApplication {
             switch (userInput) {
                 case "og-cullen-bohannon":
                     System.out.println("\nName: " + students.get("og-cullen-bohannon").getName() + " - GitHub Username: " + cullen);
-                    System.out.printf("Current Average: %.2f", student1.getGradeAverage());
+                    System.out.printf("Current Average: %.2f%%", student1.getGradeAverage());
+                    System.out.println("\nThe student was absent: " + student1.absentDays());
+                    System.out.printf("\nStudent attendance percentage: %.2f%%", student1.getAttendancePercentage());
                     break;
                 case "miss-lily-bell":
                     System.out.println("\nName: " + students.get("miss-lily-bell").getName() + " - GitHub Username: " + lily);
-                    System.out.printf("Current Average: %.2f", student2.getGradeAverage());
+                    System.out.printf("Current Average: %.2f%%", student2.getGradeAverage());
+                    System.out.println("\nThe student was absent: " + student2.absentDays());
+                    System.out.printf("\nStudent attendance percentage: %.2f%%", student2.getAttendancePercentage());
                     break;
                 case "doc-durant":
                     System.out.println("\nName: " + students.get("doc-durant").getName() + " - GitHub Username: " + durant);
-                    System.out.printf("Current Average: %.2f", student3.getGradeAverage());
+                    System.out.printf("Current Average: %.2f%%", student3.getGradeAverage());
+                    System.out.println("\nThe student was absent: " + student3.absentDays());
+                    System.out.printf("\nStudent attendance percentage: %.2f%%", student3.getAttendancePercentage());
                     break;
                 case "mister-ferguson":
                     System.out.println("\nName: " + students.get("mister-ferguson").getName() + " - GitHub Username: " + ferguson);
-                    System.out.printf("Current Average: %.2f", student4.getGradeAverage());
+                    System.out.printf("Current Average: %.2f%%", student4.getGradeAverage());
+                    System.out.println("\nThe student was absent: " + student4.absentDays());
+                    System.out.printf("\nStudent attendance percentage: %.2f%%", student4.getAttendancePercentage());
                     break;
             }
 
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,12 +1,13 @@
 package grades;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Student {
-
     private String name;
 
-    public Student (String name) {
+    public Student(String name) {
         this.name = name;
     }
 
@@ -18,12 +19,14 @@ public class Student {
         this.name = name;
     }
 
+    // array list of grades
     private ArrayList<Integer> grades = new ArrayList<>();
 
     // adds the given grade to the grades property
     public void addGrade(int grade) {
         grades.add(grade);
     }
+
 
     // returns the average of the students grades
     public double getGradeAverage() {
@@ -32,6 +35,42 @@ public class Student {
             gradeTotal += i;
         }
         return gradeTotal / grades.size();
+    }
+
+
+    //attendance HashMap    date, absent/present
+    HashMap<String, String> attendance = new HashMap<>();
+
+    public void recordAttendance(String date, String value) {
+        if (value.toLowerCase().equals("a") || value.toLowerCase().equals("absent")) {
+            attendance.put(date, "A");
+        } else if (value.toLowerCase().equals("p") || value.toLowerCase().equals("present")) {
+            attendance.put(date, "P");
+        }
+    }
+
+    // returns the average of the students grades
+    public double getAttendancePercentage() {
+        double totalDays = 0;
+        double absent = 0;
+        for (Map.Entry<String, String> entry : attendance.entrySet()) {
+            if (entry.getValue().equalsIgnoreCase("A")) {
+                absent++;
+            }
+            totalDays++;
+        }
+        return (totalDays - absent) / totalDays;
+    }
+
+    // finding specific days a student was absent
+    public ArrayList<String> absentDays () {
+        ArrayList<String> absenceList = new ArrayList<>();
+        for (Map.Entry<String, String> entry : attendance.entrySet()) {
+            if (entry.getValue().equalsIgnoreCase("a")) {
+                absenceList.add(entry.getKey());
+            }
+        }
+        return absenceList;
     }
 
 }
