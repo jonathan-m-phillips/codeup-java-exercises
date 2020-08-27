c12af90b6d01fac5c98a9f6ba6cf40cd661f650e
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-27 09:16:14 -0500

    8/27

---
+++ src/CodeupQuiz.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+public class CodeupQuiz {
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int[][] primes = new int[3][3];
+//        primes[2][2] = 1;
+//
+//        System.out.println(primes[1][2]);
+//    }
+//}
+
+
+//public class Marker {
+//    public static void main(String[] args) {
+//        List<Integer> numbers = new ArrayList<>();
+//        numbers.add(13);
+//        numbers.add(new Integer(8)); // Line 7
+//        numbers.add(3.6); // Line 8
+//        for (Integer number: numbers) System.out.println(number); // Line 9
+//    }
+//}
+
+
+
+    public static void main(String[]args){
+
+        String[] names = {"James Gosling", "Mike Sheridan", "Patrick Naughton"};
+        for (String name : names)
+            System.out.println(name);
+    }
+}
